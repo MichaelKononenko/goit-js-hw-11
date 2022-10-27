854a95d0048d316be74f31735fe2c382e3035184
Michael <michael.kononenko101@gmail.com>
2022-10-28 00:32:46 +0300

    changed requests method

--- src/js/fetchPhotos.js
+++ src/js/fetchPhotos.js
@@ -2,20 +2,21 @@ import { loadMore } from '../index.js';
 import axios from 'axios';
 
 const SEARCH_URL = 'https://pixabay.com/api/';
-const API_KEY = '30834606-0dc24151179eb34ac466f7732';
 
 async function searchPhoto(input, page) {
+  const params = {
+    key: '30834606-0dc24151179eb34ac466f7732',
+    q: input,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page,
+    per_page: 40,
+  
+  }
+
   loadMore.classList.add('is-hidden');
-  const result = await axios(
-    SEARCH_URL +
-      '?key=' +
-      API_KEY +
-      '&q=' +
-      input +
-      '&image_type=photo&orientation=horizontal&safesearch=true&page=' +
-      page +
-      '&per_page=40'
-  );
+  const result = await axios(SEARCH_URL, { params });
 
   return result.data;
 }
